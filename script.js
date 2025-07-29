dcf80a401149296a3dedc82508ced5ea4eff2adf
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-07-29 18:34:40 +0200

    Added typewriter effects on the introduction

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+// Set current year in footer
 let todaysDate = new Date();
 const currentYear = todaysDate.getFullYear();
-console.log(currentYear);
 
 const footer = document.getElementById("footer");
 footer.innerHTML = `Copyright &#169; ${currentYear} Evelyn Nomsa Mwanunura. All Rights Reserved.`;
+
+// Typewriter Effect Function
+function typeWriter(text, elementID, speed = 100) {
+  const element = document.getElementById(elementID);
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      element.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, speed);
+    }
+  }
+
+  element.textContent = ""; // Clear previous content if any
+  type();
+}
+
+// Call the function
+typeWriter("HI, I'M EVELYN NOMSA MWANUNURA", "typewriter", 100);
